a26d36c9e212bc4c90f11e25a1449e1c2dc4ac80
Hans Adema <hans@grendelhosting.com>
2015-08-12 01:28:48 +0200

    bootstrap comments

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,23 +2,31 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
+// Require the Composer Autoloader
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Setup a new Silex application
 $app = new Silex\Application();
+
+// Register Silex services
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
+// Load the config of the config.php file into the dependency container
 $config = include("./config.php");
 
 foreach($config as $key => $value){
     $app[$key] = $value;
 }
 
+// Add other stuff to the dependency container
 $app['base_path'] = $basePath;
 
+// A basic home page with links to the available theme pages
 $app->get('/', function() use ($app) {
     return include($app['base_path']."/resources/home.php");
 })->bind("home");
 
+// All relevant control panel views
 $app->get('/login', function () use ($app) {
     $controller = new \YouDesigner\Controllers\PublicController($app);
     return $controller->actionLogin();
@@ -54,6 +62,7 @@ $app->get('/createaccount', function() use ($app) {
     return $controller->actionCreate();
 })->bind('newaccount');
 
+// By default, Silex doesn't show the exception message to the user. We do want to show it.
 $app->error(function(\Exception $ex, $code) use ($app) {
     if($app['debug']){
         return;
